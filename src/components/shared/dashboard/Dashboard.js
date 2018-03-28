773f1534dac16f670c61aafb13f3e41c29301a1a
shadycrab92 <art.belichenko@gmail.com>
2018-03-28 19:16:29 +0300

    qwe

--- src/components/shared/dashboard/Dashboard.js
+++ src/components/shared/dashboard/Dashboard.js
@@ -6,7 +6,12 @@ const Dashboard = () => {
     <section className="dashboard">
       <div className="container">
         <div className="dashboard__content">
-asd
+          <a className="button is-info">
+                 <span className="icon">
+                   <i className="fas fa-pencil-alt"></i>
+                 </span>
+            <span>Написать</span>
+          </a>
         </div>
       </div>
     </section>
